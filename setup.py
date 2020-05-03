c56c7f71fdfd3ce5bf60f57b15d3ab7544082135
tobiasHeinke <tobias.heinke@outlook.com>
2020-05-03 22:32:18 +0200

    add comma

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         ],
     },
     classifiers=[
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
